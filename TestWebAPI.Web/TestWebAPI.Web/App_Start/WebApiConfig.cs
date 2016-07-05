deb2aaae08a81f702fdaaac06fe8d9deb592fb69
Ryan <Ryan@DESKTOP-1LLTILR>
2016-07-05 16:13:38 -0400

    Added test WebAPI project using WebAPIContrib RequireHttpsHandler and custom message handler for validating api key in URI query string.

---
+++ TestWebAPI.Web/TestWebAPI.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using TestWebAPI.Web.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApiContrib.MessageHandlers;
+
+namespace TestWebAPI.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services            
+            config.MessageHandlers.Add(new RequireHttpsHandler());
+            config.MessageHandlers.Add(new ApiKeyHandler("abc123"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TestWebAPI.Web/TestWebAPI.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestWebAPI.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
